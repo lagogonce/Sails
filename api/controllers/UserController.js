21f9ca06adc0b0f826b88b859993904d5aa16b41
unknown <lagogonce@gmail.com>
2014-12-11 23:41:43 +0100

    model

---
+++ api/controllers/UserController.js
@@ -0,0 +1,59 @@
+/**
+ * UserController
+ *
+ * @description :: Server-side logic for managing Users
+ * @help        :: See http://links.sailsjs.org/docs/controllers
+ */
+
+module.exports = {
+	
+
+
+  /**
+   * `UserController.index()`
+   */
+  index: function (req, res) {
+    return res.json({
+      todo: 'index() is not implemented yet!'
+    });
+  },
+
+
+  /**
+   * `UserController.new()`
+   */
+  new: function (req, res) {
+    return res.json({
+      todo: 'new() is not implemented yet!'
+    });
+  },
+
+
+  /**
+   * `UserController.create()`
+   */
+  create: function (req, res, next) {
+    var params = req.params.all();
+
+    UserPoint.create(params, function(err, UserPoint) {
+
+        if (err) return next(err);
+
+        res.status(201);
+
+        res.json(UserPoint);
+
+    });
+  },
+
+
+  /**
+   * `UserController.delete()`
+   */
+  delete: function (req, res) {
+    return res.json({
+      todo: 'delete() is not implemented yet!'
+    });
+  }
+};
+
---
+++ api/models/UserPoint.js
@@ -0,0 +1,21 @@
+/**
+* UserPoint.js
+*
+* @description :: TODO: You might write a short summary of how this model works and what it represents here.
+* @docs        :: http://sailsjs.org/#!documentation/models
+*/
+
+module.exports = {
+
+  attributes: {
+
+    lat : { type: 'float' },
+
+    long : { type: 'float' },
+
+    desc : { type: 'string' },
+
+    ts : { type: 'string' }
+  }
+};
+
