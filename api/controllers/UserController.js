b7442a7666576d5cb063c51164355b4450a8a654
Mateusz Dobek <mat.dobek@gmail.com>
2014-12-12 00:17:55 +0100

    cleaned user controller

--- api/controllers/UserController.js
+++ api/controllers/UserController.js
@@ -1,58 +1,19 @@
-/**
- * UserController
- *
- * @description :: Server-side logic for managing Users
- * @help        :: See http://links.sailsjs.org/docs/controllers
- */
-
 module.exports = {
-	
 
-
-  /**
-   * `UserController.index()`
-   */
   index: function (req, res) {
-    return res.json({
-      todo: 'index() is not implemented yet!'
+    UserPoint.find(function foundUsers(err, users) {
+      return res.json(users);
     });
   },
 
 
-  /**
-   * `UserController.new()`
-   */
-  new: function (req, res) {
-    return res.json({
-      todo: 'new() is not implemented yet!'
-    });
-  },
-
-
-  /**
-   * `UserController.create()`
-   */
   create: function (req, res, next) {
-    var params = req.params.all();
+    var user_to_create = req.params.all();
 
-    UserPoint.create(params, function(err, UserPoint) {
-
+    UserPoint.create(user_to_create, function(err, created_user) {
         if (err) return next(err);
 
-        res.status(201);
-
-        res.json(UserPoint);
-
-    });
-  },
-
-
-  /**
-   * `UserController.delete()`
-   */
-  delete: function (req, res) {
-    return res.json({
-      todo: 'delete() is not implemented yet!'
+        res.json(created_user);
     });
   }
 };
